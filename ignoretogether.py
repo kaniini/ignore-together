af9e1574c90806c44de99337a83dcb852040e4a8
William Pitcock <nenolod@dereferenced.org>
2015-03-25 13:41:44 -0500

    split out testsuite from core script

--- ignoretogether.py
+++ ignoretogether.py
@@ -89,13 +89,3 @@ class IgnoreRuleSet:
 
 rules = {}
 
-#if __name__ == '__main__':
-#    print('Simulation of commands which would be run on example-ruleset.')
-#    rs = IgnoreRuleSet('example', 'example-ruleset.yml')
-#    rs.load()
-#
-#    print('Ruleset installation.')
-#    rs.install()
-#
-#    print('Ruleset uninstallation.')
-#    rs.uninstall()
