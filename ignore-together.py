48ad378a69c4bf9b58354b5b405177124cb047fb
William Pitcock <nenolod@dereferenced.org>
2015-03-25 12:00:47 -0500

    weechat script: enhance testsuite a little

--- ignore-together.py
+++ ignore-together.py
@@ -89,6 +89,12 @@ class IgnoreRuleSet:
 rules = {}
 
 if __name__ == '__main__':
+    print('Simulation of commands which would be run on example-ruleset.')
     rs = IgnoreRuleSet('example', 'example-ruleset.yml')
     rs.load()
+
+    print('Ruleset installation.')
     rs.install()
+
+    print('Ruleset uninstallation.')
+    rs.uninstall()
