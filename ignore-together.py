f579c30ea5c21a0aef54764b4cd3b32f12323a05
William Pitcock <nenolod@dereferenced.org>
2015-03-25 13:14:22 -0500

    weechat script: make loadable in weechat

--- ignore-together.py
+++ ignore-together.py
@@ -6,6 +6,8 @@ import os
 import sys
 import yaml
 
+weechat_is_fake = False
+
 try:
     import weechat
 except:
@@ -87,13 +89,13 @@ class IgnoreRuleSet:
 
 rules = {}
 
-if __name__ == '__main__':
-    print('Simulation of commands which would be run on example-ruleset.')
-    rs = IgnoreRuleSet('example', 'example-ruleset.yml')
-    rs.load()
-
-    print('Ruleset installation.')
-    rs.install()
-
-    print('Ruleset uninstallation.')
-    rs.uninstall()
+#if __name__ == '__main__':
+#    print('Simulation of commands which would be run on example-ruleset.')
+#    rs = IgnoreRuleSet('example', 'example-ruleset.yml')
+#    rs.load()
+#
+#    print('Ruleset installation.')
+#    rs.install()
+#
+#    print('Ruleset uninstallation.')
+#    rs.uninstall()
